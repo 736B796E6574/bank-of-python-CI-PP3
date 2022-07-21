ead7f69b9fd41960117105735af4feba3fb53db7
martin-mcinerney <martin_mcinerney@live.ie>
2022-07-21 18:42:24 +0100

    Connects google sheets

--- run.py
+++ run.py
@@ -2,6 +2,20 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Bank_of_Python')
+accounts = SHEET.worksheet('Accounts')
 new_user = []
 
 def home_screen():
@@ -40,10 +54,12 @@ def add_new_balance():
     new_deposit = int(input('Please enter your deposit amount, without commas: \n'))
     print(f'You entered {new_deposit}. Updating your account...\n')
     new_user.append(new_deposit)
-    print('Deposit successfull!')
+    print('Deposit successfull!\n')
+    accounts.append_row(new_user)
+    print('Your account creation was successful!\n')
 
 
 
 home_screen()
 
-print(new_user)
+
